3cc572bf51cffff380c5726da29b2f9654ffa934
MrBenz <dkostya05@gmail.com>
2022-06-09 12:14:50 +0300

    .

--- virtual_venv/blog/admin.py
+++ virtual_venv/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BlogClubs, Mytable
+from .models import Olympiads, Mytable
 
 admin.site.register(Mytable)
-admin.site.register(BlogClubs)
+admin.site.register(Olympiads)
--- virtual_venv/blog/views.py
+++ virtual_venv/blog/views.py
@@ -13,69 +13,52 @@ def achievements(request):
 
 #class Achievements():
 def getachievements(request):
-	if "subject" in request.GET or "teacher" in request.GET or "name" in request.GET:
+	if "subject" in request.GET or "teacher" in request.GET or "event" in request.GET:
 		name_subject = request.GET.get("subject", "")
 		name_teacher = request.GET.get("teacher", "")
-		date_achievemebt = request.GET.get("name", "")
-			#if name_subject == "" and name_teacher == "" and date_achievemebt == "":
-			#	output = "{}".format(sort_data("all",[]))
-		output =  name_teacher
+		date_achievemebt = request.GET.get("event", "")
+		output =  "{}".format(sort_data_circle("subject", name_subject), sort_data_circle("teacher", name_teacher))
 	else:
-		output = "{}".format(sort_data_2())
+		output = "{}".format(sort_data_all())
 	return HttpResponse(output)
 
-def get_database(name_exel_base: str, name_base: str): # name_base - название базы которую хотим назвать
-	"""Эта функция принимает название exel таблицы и название твое базы.
-	И в результате создает файлы json и sql формата"""
-	conn = sqlite3.connect("clubs.db") # создали файл пустой базы
-	
-	cursor = conn.cursor()
-	cursor.execute("""SELECT * FROM mytable """)
-	one_result = cursor.fetchall()
-
-	subjects = dict()
-	names = [description[0] for description in cursor.description]
-	for i, value in enumerate(one_result):
-		bas = dict()
-		for j, item in enumerate(names):# создает словарь вида -> index: {id: x, some_name: name, ...}
-			bas[item] = value[j]
-		subjects[f"{i}"] = bas
-	return subjects
-
-	"""with open("clubs.json", "w") as write_file: # создаем файл json 
-								json.dump(subjects, write_file)"""
-
-def sort_data(title, objects: str):
+def get_database(title, objects: str): # name_base - название базы которую хотим назвать
 	table = []
 	b = ""
 	objects_ = re.split(',', objects)
 	for i in objects_:
-		if title == "name":
-			x = models.Mytable.objects.filter(name=i)
-		if title == "teacher":
+		pass
 
-			x = models.Mytable.objects.filter(teacher=i)
+def sort_data_circle(title, objects: str):
+	table = []
+	b = ""
+	objects_ = re.split(',', objects)
+	for i in objects_:
+		if title == "event":
+			x = models.Olympiads.objects.filter(event=i)
+		if title == "teacher":
+			x = models.Olympiads.objects.filter(head_teacher=i)
 		if title == "subject":
-			if i == "science":
-				b = "Научные"
-			elif i == "sport":
-				b = "Спортивные"
-			elif i == "game":
-				b = "Игровые"
+			if i == "physic":
+				b = "физика"
+			elif i == "economy":
+				b = "экономика"
+			elif i == "math":
+				b = "математика"
 			elif i == "info":
-				b = "Программирование"
-			x = models.Mytable.objects.filter(subject=b)
+				b = "Информатика"
+			x = models.Olympiads.objects.filter(subject=b)
 		if i == "":
 			return {}
 		else:
 			for j in range(len(x)):
-				table.append({x[j].id: [x[j].name, x[j].teacher, x[j].subject]})
+				table.append({x[j].id: [x[j].event, x[j].head_teacher, x[j].subject]})
 
 	return json.dumps(table)
 
-def sort_data_2():
+def sort_data_all():
 	table = []
-	x = models.Mytable.objects.all()
+	x = models.Olympiads.objects.all()
 	for r in range(len(x)):
-		table.append({x[r].id: [x[r].name, x[r].teacher, x[r].subject]})
+		table.append({x[r].id: [x[r].event, x[r].clas, x[r].subject]})
 	return json.dumps(table)
