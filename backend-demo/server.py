b9b8d1940ee680c391137821c9fa152c0294c9aa
Dmitri Proskuriakov <dmitri.proskuriakov@gmail.com>
2022-06-09 22:01:12 +0300

    Some front-end changes

--- backend-demo/server.py
+++ backend-demo/server.py
@@ -6,7 +6,7 @@ from flask import jsonify
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/getclubs/', methods=['GET'])
+@app.route('/getachievements/', methods=['GET'])
 def get():
     return jsonify(
         [
