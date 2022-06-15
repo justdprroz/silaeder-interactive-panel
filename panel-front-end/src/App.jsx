17084ca147678c02a9b3f771b6c58f730ada5a0e
Dmitri Proskuriakov <dmitri.proskuriakov@gmail.com>
2022-06-15 17:44:02 +0300

    Added clubs

--- panel-front-end/src/App.jsx
+++ panel-front-end/src/App.jsx
@@ -52,6 +52,7 @@ function requestData(setter, path) {
             setFilters(JSON.parse(dataReq.response)[1]);
         }
     }
+    console.log(api_url + route + "/?" + params);
     dataReq.open("GET", api_url + route + "/?" + params, true);
     dataReq.send();
 }
@@ -109,7 +110,6 @@ function Filter(props) {
 function AllFilters(props) {
     return (
         <div id="filters" class="position-absolute top-0 end-0">
-            {/* {console.log(getFilters())} */}
             <For each={Object.keys(getFilters())}>{(filter, _i) => 
                 <Filter
                     category={filter}
@@ -152,12 +152,6 @@ function ClubCard(props) {
                     Ведущий: {props.club[1]}<br/>
                     Категория: {props.club[2]}<br/>
                 </div>
-            </div>
-            <div>
-                Описание<br/>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed augue id massa egestas euismod vitae vitae ante. Nam mattis pharetra mauris, sed bibendum ipsum dictum in. Sed feugiat est in velit viverra imperdiet. Suspendisse suscipit nisi sem, id lobortis lorem mattis in. Praesent a eleifend lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam suscipit faucibus aliquam. Phasellus eu vehicula arcu.
-
-
             </div>
         </div>
     )
@@ -233,47 +227,34 @@ function Achievements() {
 }
 
 function Clubs() {
+    onMount(() => {
+        requestData(setClubs, "hobbies")
+        console.log("clubs mounted");
+    })
     return (
         <div class="h-100" style="font-family: 'efourpro'">
-            <ReturnMenu/>
-            <div id="filters" class="position-absolute top-0 end-0">
-                <Filter
-                    category={["field", "область"]}
-                    selections={[
-                        ["informatics", "информатика"],
-                        ["playing", "игровые"],
-                        ["sport", "спортивные"],
-                        ["science", "научные"]
-                    ]}
-                    setter={setClubs}
-                    path={"clubs"}
-                />
+            <div class="row">
+                <header>
+                    <ReturnMenu/>
+                    <AllFilters setter={setClubs} path="hobbies"/>
+                </header>
             </div>
-            <div class="container w-50">
-                <div class="row row-cols-2">
-                    {/* <div class="col">Column</div>
-                    <div class="col">Column</div>
-                    <div class="w-100"></div>
-                    <div class="col">Column</div>
-                    <div class="col">Column</div> */}
-                    <For each={getClubs()}>{(club, _i) =>
-                    <>
-                        <div class="col">
-                            <div style="white-space: pre; background: #ffffff; margin: 10px">
-                                    {club[_i()][0]}<br/>
-                                    {club[_i()][1]}<br/>
-                                    {club[_i()][2]}<br/>
+            <div class="row" style="font-family: 'Roboto', sans-serif; margin:0px">
+                <div class="container w-75">
+                    <div class="row row-cols-2">
+                        <For each={getClubs()}>{(club, _i) =>
+                        <>
+                            <div class="col h-25">
+                                <ClubCard club={club}/>
                             </div>
-                        </div>
-                        <Show when={_i() % 2 == 1}>
-                            <div class="w-100">separator</div>
-                        </Show>
-                    </>
-                    }</For>
+                            <Show when={_i() % 2 == 1}>
+                                <div class="w-100"></div>
+                            </Show>
+                        </>
+                        }</For>
+                    </div>
                 </div>
             </div>
-            {/* <div class="d-flex justify-content-center h-100" style="overflow: visible">
-            </div> */}
         </div>
     )
 }
@@ -282,9 +263,7 @@ function Plan() {
     return (
         <div class="h-100" style="font-family: 'efourpro'">
             <ReturnMenu/>
-            <h1 class="text-white h-100 d-flex justify-content-center align-items-center" style="font-family: 'efourpro'">
-                Plan WIP
-            </h1>
+            <img src="./assets/1.jpg" />
         </div>
     )
 }
